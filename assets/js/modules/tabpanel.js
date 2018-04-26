790366fd71f6dd270cf1eb4d2da7896826a071aa
Yvain Liechti <yliechti@clever-age.com>
2018-04-26 11:13:02 +0200

    fix-tabpanel

--- assets/js/modules/tabpanel.js
+++
@@ -1,9 +0,0 @@
-import Tablist from 'accedeweb-tablist'
-
-function TabPanel() {
-  const list = document.querySelector('[role="tablist"]')
-  console.log(list)
-  this.tablist = new window.Tablist(list)
-}
-
-export default TabPanel
